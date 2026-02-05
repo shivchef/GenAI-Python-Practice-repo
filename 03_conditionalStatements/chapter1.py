81a3091421c10a63c19ef7602cae7e320c0445d8
ShivChef <shivk10194@gmail.com>
2026-02-05 17:18:24 +0000

    Delete 03_conditionalStatements directory

--- 03_conditionalStatements/chapter1.py
+++
@@ -1,6 +0,0 @@
-
-choice = input("Enter your preferred snack:").lower()
-if choice == 'samosa' or choice == 'cookies' :
-    print(f"{choice} Order confirmed!\nThank you for your purchase.")
-else:
-    print(f"Sorry your ordered item is not available")
